4b6b820e6c7192bbe555a7012f440efaea510446
ImGunHyoeng <ynoe30@gmail.com>
2023-07-19 11:28:46 +0900

    add content

---
+++ Source/elephant/elephantGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "elephantGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELEPHANT_API AelephantGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
